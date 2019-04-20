69d75a33992ba4bb7c07e9459ae5e686d6d3b674
srijith-srinath <ssrina2@illinois.edu>
2019-04-19 19:54:54 -0500

    Adding description

---
+++ helicopter_statcalculation.py
@@ -0,0 +1,14 @@
+"""
+Simulation of Air Ambulance Requirements
+Uses basic Monte Carlo simulation techniques to derive statistical
+analysis of air ambulance requirements.
+
+IS590PR
+
+Simran Sanjiv Wig - swig2@illinois.edu
+Srijith Srinath - ssrina2@illinois.edu
+Vinu Prasad Bhambore - vpb2@illinois.edu
+
+"""
+
+
